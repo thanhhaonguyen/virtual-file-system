0a556e7a991f23d87235af5459e499845666d809
Hao Nguyen <leosnart1306@gmail.com>
2022-06-28 00:47:08 +0700

    [API] update readme

--- file-system-api/README.md
+++ file-system-api/README.md
@@ -1,6 +1,6 @@
 # file-system-api 
 
-This project uses Go, Gin Web Framework.
+This project uses Go, Gin Web Framework and Ginkgo to implement Unit Test.
 
 If you want to learn more about Gin, please visit its website: https://github.com/gin-gonic/gin .
 
@@ -8,8 +8,42 @@ If you want to learn more about Gin, please visit its website: https://github.co
 
 Docs: https://go.dev/doc
 
+## Code structure
+This project includes:
+- ```controllers:``` handle APIs logic
+- ```middlewares:``` enable CORS with various options 
+- ```models:``` include Postgres DB connection and models
+- ```utils:``` handle general errors in the app
+- ```tests:``` implement Unit Test
+
+## APIs
+### Folder
+```shell script
+[GET]     "/folder"
+[GET]     "/folder-by-parent/:id"
+[GET]     "/folder/:id"
+[POST]    "/folder"
+[PUT]     "/folder/:id"
+[DELETE]  "/folder/:id"
+```
+### File
+```shell script
+[GET]     "/file"
+[GET]     "/file/:id"
+[POST]    "/file"
+[PUT]     "/file/:id"
+[DELETE]  "/file/:id"
+```
+
 ## Running the application and visit "localhost:8080" on browser
 
 ```shell script
 go run main.go
 ```
+
+## Running Unit Test
+```shell script
+go test -v -cover ./tests/... ./controllers/v1/...
+```
+
+
--- file-system-api/tests/file-system_test.go
+++ file-system-api/tests/file-system_test.go
@@ -60,4 +60,48 @@ var _ = Describe("[file-system-api.controller]", func() {
 			})
 		})
 	})
+	Describe("Test GetFoldersByParentId", func() {
+		Context("GetFoldersByParentId with return no error", func() {
+			It("Should return message with status 200", func() {
+				// create gin context
+				req, err := http.NewRequest("GET", "/folder-by-parent/0", nil)
+				Expect(err).To(BeNil())
+				rr := httptest.NewRecorder()
+				c, _ := gin.CreateTestContext(rr)
+				c.Request = req
+
+				// mock Postgres DB connection
+				model.GetDatabaseConnection = func() (*gorm.DB, error) {
+					return dbConn, nil
+				}
+
+				// test GetFoldersByParentId
+				controller.GetFoldersByParentId(c)
+
+				// assert data
+				Expect(rr.Result().Status).To(Equal("200 OK"))
+			})
+		})
+		Context("GetFoldersByParentId with return error", func() {
+			It("Should return message with status 500", func() {
+				// create gin context
+				req, err := http.NewRequest("GET", "/folder-by-parent/0", nil)
+				Expect(err).To(BeNil())
+				rr := httptest.NewRecorder()
+				c, _ := gin.CreateTestContext(rr)
+				c.Request = req
+
+				// mock Postgres DB connection
+				model.GetDatabaseConnection = func() (*gorm.DB, error) {
+					return dbConn, fmt.Errorf("failed to get DB connection")
+				}
+
+				// test GetFoldersByParentId
+				controller.GetFoldersByParentId(c)
+
+				// assert data
+				Expect(rr.Result().Status).To(Equal("500 Internal Server Error"))
+			})
+		})
+	})
 })
